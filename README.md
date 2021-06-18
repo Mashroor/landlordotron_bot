6a2dcf558cc4eb20edb1c0d2d0d683d97a500a07
Mashroor Rashid <mrmk8&mst.edu>
2021-06-18 01:04:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Landlordotron Discord Bot
+
+## Introduction and Purpose
+
+This is a Discord bot I'm writing for my personal Discord server. The joke is that it's the landlord for my server, so it collects rent from tenants (other users), and kicks/bans them if they dont pay their rent after a month. Might add more functionality as I see fit, or if I get bored.
+
+## Technology Used (So far)
+
+* Discord API
+* Python
+
+## Command List
+
+* **'!hello'** - Standard greeting, no purpose.
+* **'!rent'** - Checks to see if tenant has paid rent for the month. Scolds if tenant has not paid.
+* **'!payRent'** - Pays rent for user, stored for a month before reset.
+* **' !domey'** - Links to https://moutonmerch.com/
+
+## Plans for later
+
+* Actually taking people money via Paypal
---
+++ landlordotron.py
@@ -0,0 +1,82 @@
+from discord.flags import Intents
+from discord.ext import tasks, commands
+from datetime import datetime
+from dotenv import load_dotenv
+import discord
+import os
+
+#Load in env file, set token
+load_dotenv()
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+userIDDictionary = {}
+
+@tasks.loop(hours = 1)
+async def monthCheckLoop():
+    #find general channel to post to
+    genChannel = client.get_channel(os.getenv("SERVER_CHANNEL_ID"))
+    if (datetime.now().day == 1):
+        await genChannel.send("It is a new month, and you know what that means. Pay your fucking rent, by using `!payRent`.")
+        #Ban everyone in userIDDictionary
+        for member in userIDDictionary:
+            if userIDDictionary[member.id] == False:
+                await member.kick(reason="Tenant behavior")
+        #Re-falsify all members
+        for member in userIDDictionary:
+           userIDDictionary[member.id] = False 
+    #2 week reminder
+    #Add formatted list of people who haven't paid
+    if (datetime.now().day == 15):
+        await genChannel.send("@everyone ATTENTION ALL RENTOIDS: Rent's due. If you haven't paid, use command `!payRent` to pay your fucking rent.")
+              
+@bot.event
+async def on_ready():
+    print("Logged in as {0.user}".format(bot))
+    #Initialize Rent dictionary from guild list
+    guild = await bot.fetch_guild(os.getenv("SERVER_ID"))
+    for member in bot.guilds[0].members:
+        userIDDictionary[member.id] = False
+    #Start rent check method
+    monthCheckLoop.start()
+
+@bot.command(
+    help="Politely greets the user",
+    brief="Greets the user"
+)
+async def hello(ctx):
+    if ctx.message.author.id != os.getenv("SERVER_OWNER_ID"):
+        await ctx.message.channel.send("Fuck off, rentoid")
+    else:
+        await ctx.message.channel.send("Hello `land-daddy` :)")
+
+@bot.command(
+    help="Check if you have paid your rent for the month",
+    brief="Check rent status"
+)
+async def rentStatus(ctx):
+    if userIDDictionary[ctx.message.author.id]:
+        await ctx.message.channel.send("You already paid your rent.")
+    else:
+        await ctx.message.channel.send("Pay your rent, you dumb fuck.")
+
+@bot.command(
+    help="Pay your rent for the month with this command",
+    brief="to pay rent"
+)
+async def payRent(ctx):
+    if userIDDictionary[ctx.message.author.id]:
+        await ctx.message.channel.send("You already paid your rent.")
+    else:
+        await ctx.message.channel.send("Thank you for paying your rent.")
+        userIDDictionary[ctx.message.author.id] = True
+
+@bot.command(
+    help="Help a small bald man pay his rent mrmL",
+    brief="Links to a good ass store"
+)
+async def domey(ctx):
+    await ctx.channel.send("https://moutonmerch.com/")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
